b2881b1800c9c8084d6d93b9d92f1a7ba39ff9ab
FREEGREAT <vladdmitrenko3009@gmail.com>
2023-06-19 20:28:55 +0300

    sort output

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useState} from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import Navbar from './components/Navbar'
-import db from './config/firebase'
 import './App.css'
-import { 
-  collection, 
-  onSnapshot,  
-  getDocs, 
-  doc,
-  query  } from '@firebase/firestore';
 import ListGroups from './components/ListGroups';
 import EditSubjects from './components/EditSubjects';
-
-
-async function queryDoc(){
-  const querr = query(
-    collection(db, '/kpfk')
-  );
-
-  const querySnapshot = await getDocs(querr);
-  const allDocs = querySnapshot.forEach((snap) => {
-    console.log(`Document ${snap.id} contains ${JSON.stringify(snap.data())}`);
-  });
-
-}
-
 
 
 
 
 function App() {
-
   const [showSidebar, setShowBar] = useState(true)
   return (
       <div className='App'>
@@ -39,7 +17,7 @@ function App() {
         </header>
          < Navbar show={showSidebar} />
         <ListGroups />
-        <EditSubjects />
+        <EditSubjects/>
       </div>
       
   )
